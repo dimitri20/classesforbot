178bfce4c936e67de47801f91e7a9362f96c1b19
dimitri20 <45402354+dimitri20@users.noreply.github.com>
2020-07-24 11:30:33 +0400

    Update wikiapi.py

--- wikiapi.py
+++ wikiapi.py
@@ -68,25 +68,3 @@ if __name__ == '__main__':
 
 
 
-
-"""
-
-def print_langlinks(page):
-        langlinks = page.langlinks
-        for k in sorted(langlinks.keys()):
-            v = langlinks[k]
-            print("%s: %s - %s: %s" % (k, v.language, v.title, v.fullurl))
-
-print_langlinks(page_py)
-# af: af - Python (programmeertaal): https://af.wikipedia.org/wiki/Python_(programmeertaal)
-# als: als - Python (Programmiersprache): https://als.wikipedia.org/wiki/Python_(Programmiersprache)
-# an: an - Python: https://an.wikipedia.org/wiki/Python
-# ar: ar - بايثون: https://ar.wikipedia.org/wiki/%D8%A8%D8%A7%D9%8A%D8%AB%D9%88%D9%86
-# as: as - পাইথন: https://as.wikipedia.org/wiki/%E0%A6%AA%E0%A6%BE%E0%A6%87%E0%A6%A5%E0%A6%A8
-
-page_py_cs = page_py.langlinks['cs']
-print("Page - Summary: %s" % page_py_cs.summary[0:60])
-# Page - Summary: Python (anglická výslovnost [ˈpaiθtən]) je vysokoúrovňový sk
-
-
-"""
